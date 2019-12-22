8a630b76cac122c5f2cb4e931d932290f1190802
Juan Dausa <juandausa@gmail.com>
2019-12-22 18:25:49 -0300

    Se agrega el cálculo de planetas alineados a WeatherControlSystem.

--- Entities/WeatherControl/Forecast.cs
+++ Entities/WeatherControl/Forecast.cs
@@ -2,6 +2,11 @@
 {
     public class Forecast
     {
+        public Forecast(uint day)
+        {
+            Day = day;
+        }
+
         public Weather Weather { get; set; }
         public uint Day { get; set; }
     }
--- Entities/WeatherControl/PointExtensions.cs
+++ Entities/WeatherControl/PointExtensions.cs
@@ -4,6 +4,11 @@ namespace Entities.WeatherControl
 {
     public static class PointExtensions
     {
+        public static (double, double) ToCartesian(this (double radius, double angle) point, int decimalPrecision)
+        {
+            return (Math.Round(point.radius * Math.Cos(point.angle * Math.PI / 180), decimalPrecision), Math.Round(point.radius * Math.Sin(point.angle * Math.PI / 180), decimalPrecision));
+        }
+
         public static double Distance(this (double x, double y) a, (double x, double y) b, int decimalPrecision)
         {
             return Math.Round(Math.Sqrt(Math.Pow((b.x - a.x), 2) + Math.Pow((b.y - a.y), 2)), decimalPrecision);
