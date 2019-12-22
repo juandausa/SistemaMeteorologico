1292cc1f6faba6b453298ea797cc4166f7960c46
Juan Dausa <juandausa@gmail.com>
2019-12-22 19:44:15 -0300

    Se agrega la entidad Triangle, el cálculo de puntos dentro del mismo y su perímetro.

---
+++ Entities.Test/WeatherControl/TriangleTestCase.cs
@@ -0,0 +1,61 @@
+﻿using Entities.WeatherControl;
+using FluentAssertions;
+using Xunit;
+
+namespace Entities.Test.WeatherControl
+{
+    public class TriangleTestCase
+    {
+        [Fact]
+        public void CreateTriangle_ReturnNoNull()
+        {
+            Triangle triangle = new Triangle((-4, -3), (-1, 4), (6, 1));
+            triangle.Should().NotBeNull();
+        }
+
+
+        [Fact]
+        public void TriangleWithOriginInside_ContainsOrigin_ReturnTrue()
+        {
+
+            Triangle triangle = new Triangle((-4, -3), (-1, 4), (6, 1));
+            triangle.Contains((0, 0)).Should().BeTrue();
+            triangle.Contains((-11, 9)).Should().BeFalse();
+        }
+
+        [Fact]
+        public void TriangleWithOriginInside_NotContains533_ReturnFalse()
+        {
+
+            Triangle triangle = new Triangle((-4, -3), (-1, 4), (6, 1));
+            triangle.Contains((5, 33)).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Triangle_ContainsVertex_ReturnTrue()
+        {
+
+            Triangle triangle = new Triangle((-4, -3), (-1, 4), (6, 1));
+            triangle.Contains((-4, -3)).Should().BeTrue();
+            triangle.Contains((-1, 4)).Should().BeTrue();
+            triangle.Contains((6, 1)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Triangle_GetPerimeter_ReturnPerimeter()
+        {
+            var triangle = new Triangle((1, 1), (3, 4), (9, 2), 4);
+            triangle.Perimeter.Should().Be(17.9925);
+        }
+
+        [Fact]
+        public void TrianglesWithSameVertex_GetPerimeter_ReturnPerimeter()
+        {
+            var triangle = new Triangle((6, 0), (0, 0), (3, 3), 4);
+            triangle.Perimeter.Should().Be(14.4852);
+
+            var anotherTriangle = new Triangle((0, 0), (3, 3), (6, 0), 4);
+            anotherTriangle.Perimeter.Should().Be(14.4852);
+        }
+    }
+}
---
+++ Entities/WeatherControl/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Entities.WeatherControl
+{
+    public class Triangle
+    {
+        public (double x, double y) A { get; }
+        public (double x, double y) B { get; }
+        public (double x, double y) C { get; }
+        public int DecimalPresicion { get; }
+        public double Perimeter { get; }
+
+        public Triangle((double x, double y) a, (double x, double y) b, (double x, double y) c, int decimalPresicion)
+        {
+            if (decimalPresicion < 0)
+            {
+                throw new ArgumentException(nameof(decimalPresicion));
+            }
+
+            this.DecimalPresicion = decimalPresicion;
+            this.A = a;
+            this.B = b;
+            this.C = c;
+            this.Perimeter = Math.Round(this.A.Distance(this.B, this.DecimalPresicion) + this.B.Distance(this.C, this.DecimalPresicion) + this.C.Distance(this.A, this.DecimalPresicion), this.DecimalPresicion);
+        }
+
+        public Triangle((double x, double y) a, (double x, double y) b, (double x, double y) c) : this(a, b, c, 2) { }
+
+        /// <summary>
+        /// Returns true if the point is within the triangle, false otherwise.
+        /// Based on http://www.dma.fi.upm.es/personal/mabellanas/tfcs/kirkpatrick/Aplicacion/algoritmos.htm#puntoInteriorDefinicion
+        /// </summary>
+        /// <param name="point">The point</param>
+        /// <returns>True if the point is within the triangle, false otherwise</returns>
+        public bool Contains((double x, double y) point)
+        {
+            if (new Collection<(double, double)>() { this.A, this.B, this.C }.Contains(point))
+            {
+                return true;
+            }
+
+            var originalDirection = this.CalculateDirection(this.A, this.B, this.C);
+            return originalDirection == this.CalculateDirection(this.A, this.B, point) && originalDirection == this.CalculateDirection(this.B, this.C, point) && originalDirection == this.CalculateDirection(this.C, this.A, point);
+        }
+
+        private int CalculateDirection((double x, double y) a, (double x, double y) b, (double x, double y) c)
+        {
+            return Math.Sign((a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x));
+        }
+    }
+}
