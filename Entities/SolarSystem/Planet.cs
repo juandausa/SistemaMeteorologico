0ad075322c3cf01929d3b5ad4cde85bf4c39dbe9
Juan Dausa <juandausa@gmail.com>
2019-12-22 17:06:47 -0300

    Se agrega la estructura de respuesta del reporte del tiempo.

--- Entities/SolarSystem/Planet.cs
+++ Entities/SolarSystem/Planet.cs
@@ -2,16 +2,16 @@
 {
     public class Planet
     {
+        public string Name { get; }
+        public decimal Velocity { get; }
+        public decimal SunDistance { get; }
+
         public Planet(string name, decimal sunDistance, decimal velocity)
         {
-            this.Name = name;
+            this.Name = name ?? throw new System.ArgumentNullException(nameof(name));
             this.SunDistance = sunDistance;
             this.Velocity = velocity;
         }
-
-        public string Name { get; set; }
-        public decimal Velocity { get; set; }
-        public decimal SunDistance { get; set; }
 
         public decimal GetPosition(uint day)
         {
