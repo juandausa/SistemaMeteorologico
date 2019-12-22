38361f9c4daf8a3c4a5a25093484c55538f73531
Juan Dausa <juandausa@gmail.com>
2019-12-22 18:46:56 -0300

    Se agrega extrae la presicion de cálculo de SolarSystem y se lleva a WeatherControlSystem.

--- Entities.Test/WeatherControl/WeatherControlSystemTestCase.cs
+++ Entities.Test/WeatherControl/WeatherControlSystemTestCase.cs
@@ -24,7 +24,7 @@ namespace Entities.Test.WeatherControl
         }
 
         [Fact]
-        public void DroughtSeasonSolarSystem_GetForecast_ReturnDroughtSeasson()
+        public void DroughtSeasonSolarSystem_GetForecast_ReturnDroughtSeason()
         {
             var earth = new Planet("Earth", 1000, 0);
             var mars = new Planet("Mars", 2000, 0);
@@ -36,6 +36,21 @@ namespace Entities.Test.WeatherControl
             var forecast = weatherControlSystem.CalculateForecast(1);
             forecast.First().Day.Should().Be(1);
             forecast.First().Weather.Should().Be(Weather.Drought);
+        }
+
+        [Fact]
+        public void NormalSeasonSolarSystem_GetForecast_ReturnNormalSeason()
+        {
+            var earth = new Planet("Earth", 1000, 45);
+            var mars = new Planet("Mars", 1000, 45);
+            var neptune = new Planet("Neptune", 1000, -45);
+            var planets = new List<Planet>() { earth, mars, neptune };
+
+            var solarSystem = new Entities.SolarSystem.SolarSystem(planets);
+            var weatherControlSystem = new WeatherControlSystem(solarSystem);
+            var forecast = weatherControlSystem.CalculateForecast(1);
+            forecast.First().Day.Should().Be(1);
+            forecast.First().Weather.Should().Be(Weather.Normal);
         }
     }
 }
