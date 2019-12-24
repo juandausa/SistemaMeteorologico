9da7152536940fb0adc5897890ba1a504f6b0f6d
Juan Dausa <juandausa@gmail.com>
2019-12-24 15:51:55 -0300

    Se agregan pruebas unitarias para ForecastReport.

--- Entities.Test/WeatherControl/ForecastReportTestCase.cs
+++ Entities.Test/WeatherControl/ForecastReportTestCase.cs
@@ -97,5 +97,73 @@ namespace Entities.Test.WeatherControl
             var report = new ForecastReport(forecast);
             report.HeaviestDayOfRain.Should().Be(1);
         }
+
+        [Fact]
+        public void OneDayOfRainyAndOneOfDrouthForecast_GenerateReport_ReturnTwoPeriods()
+        {
+            var forecast = new List<Forecast>()
+            {
+                new Forecast(1, 321),
+                new Forecast(2, Weather.Drought),
+            };
+
+            var report = new ForecastReport(forecast);
+            report.Periods.Values.Any(value => value > 0).Should().BeTrue();
+            report.Periods.Values.Count(value => value > 0).Should().Be(2);
+            report.Periods[Weather.Rainy].Should().Be(1);
+            report.Periods[Weather.Drought].Should().Be(1);
+        }
+
+        [Fact]
+        public void OneDayOfRainyAndOneOfNormalForecast_GenerateReport_ReturnTwoPeriods()
+        {
+            var forecast = new List<Forecast>()
+            {
+                new Forecast(1, 325),
+                new Forecast(2, Weather.Normal),
+            };
+
+            var report = new ForecastReport(forecast);
+            report.Periods.Values.Any(value => value > 0).Should().BeTrue();
+            report.Periods.Values.Count(value => value > 0).Should().Be(2);
+            report.Periods[Weather.Rainy].Should().Be(1);
+            report.Periods[Weather.Normal].Should().Be(1);
+        }
+
+        [Fact]
+        public void TenDaysOfRainyAndOneOfNormalForecast_GenerateReport_ReturnTwoPeriods()
+        {
+            List<Forecast> forecast = CreateTenDaysOfRain();
+            forecast.Add(new Forecast(11, Weather.Normal));
+            var report = new ForecastReport(forecast);
+            report.Periods.Values.Any(value => value > 0).Should().BeTrue();
+            report.Periods.Values.Count(value => value > 0).Should().Be(2);
+            report.Periods[Weather.Rainy].Should().Be(1);
+            report.Periods[Weather.Normal].Should().Be(1);
+        }
+
+        [Fact]
+        public void TwoPeriodsOfRainyAndOneOfNormalForecast_GenerateReport_ReturnPeriods()
+        {
+            List<Forecast> forecast = CreateTenDaysOfRain();
+            forecast.Add(new Forecast(11, Weather.Normal));
+            forecast.Add(new Forecast(12, 100));
+            var report = new ForecastReport(forecast);
+            report.Periods.Values.Any(value => value > 0).Should().BeTrue();
+            report.Periods.Values.Count(value => value > 0).Should().Be(2);
+            report.Periods[Weather.Rainy].Should().Be(2);
+            report.Periods[Weather.Normal].Should().Be(1);
+        }
+
+        private static List<Forecast> CreateTenDaysOfRain()
+        {
+            var forecast = new List<Forecast>();
+            for (uint i = 1; i < 10; i++)
+            {
+                forecast.Add(new Forecast(i, i + 200));
+            }
+
+            return forecast;
+        }
     }
 }
