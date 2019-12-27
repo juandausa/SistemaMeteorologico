223b3b556a0017c6e43c8d042dd071846f2493af
Juan Dausa <juandausa@gmail.com>
2019-12-26 22:43:41 -0300

    Se actualiza la firma del método de consulta del clima.

--- Entities/WeatherControl/Weather.cs
+++ Entities/WeatherControl/Weather.cs
@@ -1,10 +1,16 @@
-﻿namespace Entities.WeatherControl
+﻿using System.ComponentModel;
+
+namespace Entities.WeatherControl
 {
     public enum Weather
     {
+        [Description("sequia")]
         Drought = 0,
+        [Description("lluvia")]
         Rainy = 1,
+        [Description("condiciones normales de presión y temperatura")]
         Normal = 2,
+        [Description("otro")]
         Other = 3
     }
 }
