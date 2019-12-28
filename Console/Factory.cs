71764f670d621395f318c22f6460c257963127a1
Juan Dausa <juandausa@gmail.com>
2019-12-28 14:29:56 -0300

    Se agrega nuevamente el proyecto de consola.

---
+++ Console/Factory.cs
@@ -0,0 +1,18 @@
+﻿using Entities.SolarSystem;
+using System.Collections.Generic;
+
+namespace Console
+{
+    public static class Factory
+    {
+        public static SolarSystem GenerateSolarSystem()
+        {
+            var ferengi = new Planet("Ferengi", 1, 500);
+            var betasoide = new Planet("Betasoide", 3, 2000);
+            var vulcano = new Planet("Vulcano", -5, 1000);
+
+            return new SolarSystem(new List<Planet>() { ferengi, betasoide, vulcano });
+        }
+
+    }
+}
